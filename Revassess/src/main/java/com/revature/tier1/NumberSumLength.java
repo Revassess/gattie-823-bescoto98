c9363dc6114811d04e457ba828d20cb2d0b5e9e8
bescoto98 <escoto.brenda42@gmail.com>
2020-08-26 11:45:25 -0700

    changes

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,26 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
-		return false;
+		
+		StringBuilder numStr = new StringBuilder();
+		
+		numStr.append(num);
+		
+		String number = numStr.toString();
+		
+		int numLength = number.length();
+		int sum = 0;
+		
+		for(int i =0; i<number.length(); i++) {
+			
+			int temp = Integer.parseInt(String.valueOf(number.charAt(i)));
+			
+			temp = (int) Math.pow(temp, numLength);
+			
+			sum += temp;
+			
+		}
+		
+		return sum==num;
 	}
 }
--- Revassess/src/main/java/com/revature/tier1/SumOverArray.java
+++ Revassess/src/main/java/com/revature/tier1/SumOverArray.java
@@ -3,6 +3,20 @@ package com.revature.tier1;
 public class SumOverArray {
 
 	public static int IterateAndSum(int[] arr) {
-		return 0;
+		int sum = 0;
+		try {
+			
+			
+			for(int s : arr) {
+				sum += s;
+			}
+			return sum;
+		}
+		catch(NullPointerException e) {
+			return 0;
+		}
+	
+		
+		
 	}
 }
